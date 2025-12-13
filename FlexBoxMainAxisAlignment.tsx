4954d481474b95d8a3034eefef6b763d31c16b99
Yugandhar.V <yugandharvadlamudi306@gmail.com>
2025-12-13 10:37:53 +0530

    add additional Flexbox layout examples and demonstrate alignment properties

--- FlexBoxMainAxisAlignment.tsx
+++ FlexBoxMainAxisAlignment.tsx
@@ -34,8 +34,46 @@ function FlexBoxMainAxisAlignment() {
                 <Text>ALign items</Text>
 
             </View>
-            <View style={{flexDirection: 'row',  backgroundColor: 'yellow'}}>
-                <Text style={{flex:2,backgroundColor:'red'}}> ALigne item </Text>
+            <View style={{flexDirection: 'row', backgroundColor: 'yellow'}}>
+                <Text style={{flex: 2, backgroundColor: 'red'}}> ALigne item </Text>
+            </View>
+            <View style={{flexDirection: 'row', backgroundColor: 'purple'}}>
+                <Text style={{flex: 1, backgroundColor: 'white'}}>flex 1</Text>
+                <Text style={{flex: 2, backgroundColor: 'green'}}>flex 2</Text>
+                <Text style={{flex: 1}}>flex 2</Text>
+            </View>
+            <View style={{flexDirection: 'row', justifyContent: "center"}}>
+                <Text>Center 1</Text>
+                <Text>Center 2</Text>
+                <Text>Center 3</Text>
+            </View>
+            <View style={{flexDirection: 'row', justifyContent: 'space-evenly'}}>
+                <Text>Space Evenly 1</Text>
+                <Text>Space Evenly 2</Text>
+                <Text>Space Evenly 3</Text>
+            </View>
+            <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+                <Text>Space Between 1</Text>
+                <Text>Space Between 2</Text>
+                <Text>Space Between 3</Text>
+            </View>
+            <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
+                <Text>Space around 1</Text>
+                <Text>Space around 2</Text>
+                <Text>Space around 3</Text>
+            </View>
+            {/*Dimenctions, Margins, padding, */}
+            <View style={{width: 50, height: 50, backgroundColor: 'green'}}>
+                <Text>Width Align </Text>
+            </View>
+            {/*Percentage base, padding , wdith */}
+            <View style={{width:100, height:100, backgroundColor: 'blue', padding
+            :10, margin:10}}>
+                <Text style={{width:'50%', height:'50%', backgroundColor:'purple'}}>percentage </Text>
+            </View>
+            {/*min width and height example*/}
+            <View style={{minHeight:10,minWidth:10,maxWidth:60, backgroundColor:'orange'}}>
+                <Text>Min Height</Text>
             </View>
         </View>
     )
